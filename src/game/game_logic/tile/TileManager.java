a52a65b2c702f57a8011364e693eeb2403943039
yotemaus <185620894+yotemaus@users.noreply.github.com>
2025-10-20 13:16:19 +0200

    changed the map again, added more tiles

--- src/game/game_logic/tile/TileManager.java
+++ src/game/game_logic/tile/TileManager.java
@@ -6,6 +6,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+
 import javax.imageio.ImageIO;
 
 /**
@@ -25,7 +28,7 @@ public class TileManager {
 
         this.panel = panel;
 
-        tile = new Tile[10];
+        tile = new Tile[20];
         mapTileNum = new int[panel.maxScreenRow][panel.maxScreenCol];
 
         getTileImage();
@@ -36,22 +39,75 @@ public class TileManager {
      * Stores tile images in an array for later use.
      */
     public void getTileImage() {
+
         try {
             tile[0] = new Tile();
             tile[0].image = ImageIO.read(getClass().getResourceAsStream(
-                "/Tiles/Grass_Middle.png"));
+                "/tiles/Grass_Middle.png"));
 
             tile[1] = new Tile();
             tile[1].image = ImageIO.read(getClass().getResourceAsStream(
-                "/Tiles/Path_Tile_left.png"));
+                "/tiles/path/Path_Tile_left.png"));
 
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(getClass().getResourceAsStream(
-                "/Tiles/Path_Tile_right.png"));
+                "/tiles/path/Path_Tile_right.png"));
 
             tile[3] = new Tile();
             tile[3].image = ImageIO.read(getClass().getResourceAsStream(
-                "/Tiles/Path_Tile_right.png"));  
+                "/tiles/path/Path_Tile_top.png"));
+                
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_bottom.png"));
+
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_BL_corner.png"));
+
+            tile[6] = new Tile();
+            tile[6].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_BR_corner.png"));
+
+            tile[7] = new Tile();
+            tile[7].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_TL_corner.png"));
+
+            tile[8] = new Tile();
+            tile[8].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_TR_corner.png"));
+
+            tile[9] = new Tile();
+            tile[9].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_up_left.png"));
+
+            tile[10] = new Tile();
+            tile[10].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_up_right.png"));
+
+            tile[11] = new Tile();
+            tile[11].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_down_left.png"));
+
+            tile[12] = new Tile();
+            tile[12].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_down_right.png"));
+
+            tile[13] = new Tile();
+            tile[13].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_texture_1.png"));
+
+            tile[14] = new Tile();
+            tile[14].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_texture_2.png"));
+
+            tile[15] = new Tile();
+            tile[15].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_texture_3.png"));
+
+            tile[16] = new Tile();
+            tile[16].image = ImageIO.read(getClass().getResourceAsStream(
+                "/tiles/path/Path_Tile_center.png"));
             
         } catch (IOException e) {
             e.printStackTrace();
