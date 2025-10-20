d1f76c22974697336822891194e3984be6f12c9b
yotemaus <185620894+yotemaus@users.noreply.github.com>
2025-10-20 10:42:24 +0200

    changed the map

--- src/game/game_logic/tile/TileManager.java
+++ src/game/game_logic/tile/TileManager.java
@@ -48,6 +48,11 @@ public class TileManager {
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(getClass().getResourceAsStream(
                 "/Tiles/Path_Tile_right.png"));
+
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream(
+                "/Tiles/Path_Tile_right.png"));  
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
